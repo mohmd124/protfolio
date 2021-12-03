cd30b18e09094327b4a96a99f039956f374b51ee
mohmd124 <mohmd.124@gmail.com>
2021-12-03 16:36:42 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progrssBarPercents = [97,89,87,70,75,60];
+
+window.addEventListener("scroll",()=>{
+
+  mainFn();
+})
+
+const mainFn = () => {
+    if(window.pageYOffset >= navbarOffsetTop){
+          navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+    
+
+    sections.forEach((section,i) => {
+        if(window.pageYOffset >= section.offsetTop -10) {
+            navbarLinks.forEach(navbarLink=>{
+                navbarLink.classList.remove("change");
+            })
+            navbarLinks[i].classList.add("change");
+        }
+    })
+
+    if(window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll(".progress-percent").forEach((el,i)=>{
+            el.style.width=`${progrssBarPercents[i]}%`;
+            el.previousElementSibling.firstElementChild
+            .textContent = progrssBarPercents[i];
+        });
+    }
+
+}
+
+mainFn();
+
+window.addEventListener('resize', () => {
+    window.location.reload();
+});
